c515b15d6cd890f837465a95c7796f7498d48dcf
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-03-21 12:10:51 +0000

    Added check to make sure we weren't poluting the path

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 @echo off
 
-echo %USERNAME%
-
 if exist "C:\Program files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build" (
     set "VCVARALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
 )
@@ -9,7 +7,13 @@ if exist "C:\Program files (x86)\Microsoft Visual Studio\2017\Professional\VC\Au
     set "VCVARALLDIR=C:\Program files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build"
 )
 
+REM check if msbuild is already in path
+call msbuild.exe "/ver"
+if %ERRORLEVEL% equ 0 goto :STARTBUILD
+
 call "%VCVARALLDIR%\vcvarsall.bat" x64
+
+:STARTBUILD
 
 REM Building the Beckhoff Builder
 call msbuild.exe /p:Configuration=Release;Platform=x64 util_scripts/Builder/BeckhoffBuilder.sln
