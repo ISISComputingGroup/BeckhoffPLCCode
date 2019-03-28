a0dfb03921c0f162d6a8603f9e0b981ea5a7f33a
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-03-28 19:45:36 +0000

    build rather than run

--- build.bat
+++ build.bat
@@ -21,7 +21,7 @@ call msbuild.exe /p:Configuration=Release;Platform=x64 util_scripts/Builder/Beck
 if %ERRORLEVEL% neq 0 goto :PROBLEM
 
 REM call .\util_scripts\Builder\bin\x64\Release\BeckhoffBuilder.exe "%~dp0\PLC Development.sln"
-call .\util_scripts\Builder\bin\x64\Release\BeckhoffBuilder.exe "%~dp0\dummy_PLC\TestPLC.sln" run
+call .\util_scripts\Builder\bin\x64\Release\BeckhoffBuilder.exe "%~dp0\dummy_PLC\TestPLC.sln" build
 
 if %ERRORLEVEL% neq 0 goto :PROBLEM
 
